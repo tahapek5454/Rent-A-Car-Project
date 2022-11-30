b7f88e6cada12feb3bb95f3631a33096866a1b04
tahapek5454 <tahapek5454@gmail.com>
2022-11-30 15:26:49 +0300

    add new entities and db

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -1,12 +1,15 @@
 ﻿using Entities.Abstract;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Entities.Concrete
 {
     public class Car:IEntity
     {
+        [Key]
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
---
+++ Entities/Concrete/Color.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Color:IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
