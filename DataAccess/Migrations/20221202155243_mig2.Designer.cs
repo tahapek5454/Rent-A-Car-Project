cb0d90ddca89f04423fb19799ede80ce66760c04
tahapek5454 <tahapek5454@gmail.com>
2022-12-02 18:56:28 +0300

    add foreign key to tables

--- DataAccess/Migrations/20221202155243_mig2.Designer.cs
+++ DataAccess/Migrations/20221202155243_mig2.Designer.cs
@@ -3,14 +3,16 @@ using DataAccess.Concrete.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(RentACarContext))]
-    partial class RentACarContextModelSnapshot : ModelSnapshot
+    [Migration("20221202155243_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20221202155243_mig2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_BrandId",
+                table: "Cars",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_ColorId",
+                table: "Cars",
+                column: "ColorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars",
+                column: "BrandId",
+                principalTable: "Brands",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Colors_ColorId",
+                table: "Cars",
+                column: "ColorId",
+                principalTable: "Colors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Brands_BrandId",
+                table: "Cars");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Colors_ColorId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_BrandId",
+                table: "Cars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_ColorId",
+                table: "Cars");
+        }
+    }
+}
